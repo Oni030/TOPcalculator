ac42c630fe594adf0814de8bdbf6a602d4d9ebbf
Oni030 <51912581+Oni030@users.noreply.github.com>
2024-07-02 21:12:34 +0200

    Add operate function.
    Operate is used to call the right math function with the numbers the user put in.

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let num1 = null;
+let num2 = null;
+let operator = null;
+
 function add(a, b) {
     return a + b;
 }
@@ -10,3 +14,21 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 }
+
+function operate(num1, num2, operator) {
+    if (operator === null || operator === undefined) {
+        return alert('No operator specified');
+    };
+    if (operator === '+') {
+        return add(num1, num2);
+    };
+    if (operator === '-') {
+        return subtract(num1, num2);
+    };
+    if (operator === '*') {
+        return multiply(num1, num2);
+    };
+    if (operator === '/') {
+        return divide(num1, num2);
+    };
+};
